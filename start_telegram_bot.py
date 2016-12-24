6898c1f64d404c7c55ff3f2ab9de96ea8955c43d
Avi Oza <avioza@gmail.com>
2016-12-24 15:25:44 -0500

    Add example of job handler and update bot config

--- start_telegram_bot.py
+++ start_telegram_bot.py
@@ -1,22 +1,29 @@
 import ConfigParser
 import logging
-from telegram.ext import Updater
+from telegram.ext import Job, Updater, CommandHandler
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 config = ConfigParser.ConfigParser()
 config.read('bot.config')
 
 updater = Updater(token=config.get('KEYS', 'bot_api'))
 dispatcher = updater.dispatcher
+job_queue = updater.job_queue
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+def callback_minute(bot, job):
+    bot.sendMessage(chat_id=config.get('ADMIN', 'id'), 
+        text='One message every minute')
 
 def start(bot, update):
     logging.info("Got a message")
     bot.sendMessage(chat_id=update.message.chat_id, text="TESTING FROM AVI")
 
-from telegram.ext import CommandHandler
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
+
+job_minute = Job(callback_minute, 60.0)
+job_queue.put(job_minute, next_t=0.0)
 
 updater.start_polling()
 updater.idle()
