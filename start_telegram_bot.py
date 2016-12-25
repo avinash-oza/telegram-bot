5abaa391cfb99158ba1aaf16904ffedc41dfc682
Avi Oza <avioza@gmail.com>
2016-12-24 22:54:56 -0500

    Add ability to pull information from UPS

--- start_telegram_bot.py
+++ start_telegram_bot.py
@@ -1,5 +1,6 @@
 import ConfigParser
 import logging
+import subprocess
 from telegram.ext import Job, Updater, CommandHandler
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -15,15 +16,17 @@ def callback_minute(bot, job):
     bot.sendMessage(chat_id=config.get('ADMIN', 'id'), 
         text='One message every minute')
 
-def start(bot, update):
-    logging.info("Got a message")
-    bot.sendMessage(chat_id=update.message.chat_id, text="TESTING FROM AVI")
+def power_status(bot, update, args):
+    ip_address = config.get('ADMIN', 'ups_ip') # the ip of the UPS server
+    command_to_run = ['/usr/local/nagios/libexec/check_nrpe', '-c', 'show_ups', '-H', ip_address]
+    text_output = subprocess.check_output(command_to_run)
+    bot.sendMessage(chat_id=update.message.chat_id, text=text_output)
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+power_status_handler = CommandHandler('powerStatus', power_status, pass_args=True)
+dispatcher.add_handler(power_status_handler)
 
-job_minute = Job(callback_minute, 60.0)
-job_queue.put(job_minute, next_t=0.0)
+#job_minute = Job(callback_minute, 60.0)
+#job_queue.put(job_minute, next_t=0.0)
 
 updater.start_polling()
-updater.stop()
+updater.idle()
