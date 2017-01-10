81e8ae949f685fdbde73ff01a4c1f085ff806d25
Avi Oza <avioza@gmail.com>
2017-01-09 21:04:32 -0500

    Test alert code

--- insert_alert.py
+++ insert_alert.py
@@ -28,16 +28,26 @@ def insert_new_entry(service_alert):
     # Determine how to format the string
     string_to_insert = None
     env = os.environ
+    # Parameters for alerts
+    notification_type=env.get('NAGIOS_NOTIFICATIONTYPE')
+    host_name = env.get('NAGIOS_HOSTNAME')
+    host_state = env.get('NAGIOS_HOSTSTATE')
+    host_address = env.get('NAGIOS_HOSTADDRESS')
+    service_output = env.get('NAGIOS_SERVICEOUTPUT')
+    host_output = env.get('NAGIOS_HOSTOUTPUT')
+    nagios_date_time = env.get('NAGIOS_LONGDATETIME')
+    service_name = env.get('NAGIOS_SERVICEDISPLAYNAME') # The display name of the service
+
     print env
     if service_alert:
         print "Service called"
-        string_to_insert = MESSAGE_TEXT.format(NOTIFICATIONTYPE=env.get('NAGIOS_NOTIFICATIONTYPE'), HOSTNAME=env.get('NAGIOS_HOSTNAME'), HOSTSTATE=env.get('NAGIOS_HOSTSTATE'),
-                                                        HOSTADDRESS=env.get('NAGIOS_HOSTADDRESS'), HOSTOUTPUT=env.get('NAGIOS_SERVICEOUTPUT'), LONGDATETIME=env.get('NAGIOS_LONGDATETIME'))
+        string_to_insert = MESSAGE_TEXT.format(NOTIFICATIONTYPE=notification_type, HOSTNAME=host_name, HOSTSTATE=host_state,
+                                                        HOSTADDRESS=host_address, HOSTOUTPUT=service_output, LONGDATETIME=nagios_date_time)
     else:
         # Host alert
         print "Host alert"
-        string_to_insert = MESSAGE_TEXT.format(NOTIFICATIONTYPE=env.get('NAGIOS_NOTIFICATIONTYPE'), HOSTNAME=env.get('NAGIOS_HOSTNAME'), HOSTSTATE=env.get('NAGIOS_HOSTSTATE'),
-                                                        HOSTADDRESS=env.get('NAGIOS_HOSTADDRESS'), HOSTOUTPUT=env.get('NAGIOS_HOSTOUTPUT'), LONGDATETIME=env.get('NAGIOS_LONGDATETIME'))
+        string_to_insert = MESSAGE_TEXT.format(NOTIFICATIONTYPE=notification_type, HOSTNAME=host_name, HOSTSTATE=host_state,
+                                                        HOSTADDRESS=host_address, HOSTOUTPUT=host_output, LONGDATETIME=nagios_date_time)
 
     db_host_name = config.get('DATABASE', 'host')
     db_user_name = config.get('DATABASE', 'user')
@@ -47,7 +57,8 @@ def insert_new_entry(service_alert):
     conn =  mysql.connector.connect(user=db_user_name,password=db_password,host=db_host_name, database=database_name)
     cursor = conn.cursor(buffered=True)
 
-    query =  """INSERT INTO `nagios_alerts`(`message_text`) VALUES ("{0}")""".format(string_to_insert)
+    query =  """INSERT INTO `nagios_alerts`(`message_text`, `hostname`, `service_name`, `notification_type`) VALUES ("{0}", "{1}", "{2}", "{3}")""".format(string_to_insert, host_name, service_name, notification_type)
+    print query
     cursor.execute(query )
     # Commit the changes
     conn.commit()
