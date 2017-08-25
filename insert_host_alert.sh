a42ad9a1c444d6d0a90177348c8469e56c838090
Avi Oza <avioza@gmail.com>
2017-08-24 20:10:34 -0400

    Fix paths

--- insert_host_alert.sh
+++ insert_host_alert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /home/telegram-bot/telegram-virtual-env/bin/activate
-cd /home/telegram-bot/telegram-bot
+source /home/telegram_bot/telegram-virtual-env/bin/activate
+cd /home/telegram_bot/telegram-bot
 python insert_alert.py
 deactivate
