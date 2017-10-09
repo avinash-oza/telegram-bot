896c2122f6f8aedd75405d00e6d5fa7a94fe587d
Avi Oza <avioza@gmail.com>
2017-10-09 00:02:56 -0400

    Initial (possibly unworking code of using HTTP API for getting and acknowledging alerts)

--- start_telegram_bot.py
+++ start_telegram_bot.py
@@ -21,6 +21,7 @@ import requests
 
 cache = ExpiringDict(max_len=10, max_age_seconds=15)
 market_cap_cache = ExpiringDict(max_len=10, max_age_seconds=60*5) # 5 mins
+acknowledgeable_alerts_cache = ExpiringDict(max_len=6, max_age_seconds=60) # max alerts at a time is 5
 
 # Declare states for garage door opening
 class GarageConversationState(Enum):
@@ -35,6 +36,7 @@ class TelegramBot(object):
         self.updater = Updater(token=self.config.get('KEYS', 'bot_api'))
         self.dispatcher = self.updater.dispatcher
         self.job_queue = self.updater.job_queue
+        #TODO: Maybe make this an expiring dictionary of alert ids
         self.key_to_alert_id_mapping = [] # Stores the list of keys that have been sent out at the latest alert
         self.garage_expire_request = None # job to handle expiring the codes if a garage is not selected
         self.logger = None
@@ -73,75 +75,55 @@ class TelegramBot(object):
         """
         Retrieves alerts and sends them
         """
-        # Before starting, clear out any alerts we were waiting for acknoledgement on
-        self.key_to_alert_id_mapping[:] = []
         admin_id = self.config.get('ADMIN', 'id')
         self.logger.info("Getting alerts from db")
 
-        # Open the database
-        db_host_name = self.config.get('DATABASE', 'host')
-        db_user_name = self.config.get('DATABASE', 'user')
-        db_password = self.config.get('DATABASE', 'password')
-        database_name = self.config.get('DATABASE', 'database')
+        hostname = self.config.get('ALERTS', 'hostname')
+        url = 'http://{0}/get_nagios_unsent_alerts'.format(hostname, alert_id)
+        r = requests.get(url)
+        if r.status_code != 200:
+            bot.sendMessage(chat_id=sender_id, text='An exception occured while acknowledging alert',
+                            reply_keyboard=None)
 
-        conn =  mysql.connector.connect(user=db_user_name,password=db_password,host=db_host_name, database=database_name)
-        cursor = conn.cursor(buffered=True)
-        c = conn.cursor(buffered=True)
-        write_cursor = conn.cursor(buffered=True)
-        # Get the current unsent alerts. Make sure to send them in order
-        c.execute("SELECT id date_inserted,date_sent,message_text,status,hostname,service_name, notification_type FROM nagios_alerts WHERE STATUS='UNSENT' ORDER BY id ASC")
-        # Enumerate the counter so we know how many results returned
-        results = [one_result for one_result in c]
-        total_count_of_alerts = len(results)
-        if not total_count_of_alerts:
             return ConversationHandler.END
 
-        self.logger.info("Got {0} results".format(total_count_of_alerts))
-        message_str = """"""
+        unsent_alerts = r.json()
+        if not unsent_alerts: # Nothing to do
+            return ConversationHandler.END
 
-        # Send the alerts which are not sent
-        for index, one_alert in enumerate(results):
-            # First update the alert to be SENT
-            alert_id = one_alert[0]
-            alert_text = one_alert[2]
-            host_name = one_alert[4]
-            service_name = one_alert[5]
-            notification_type = one_alert[6]
-            write_cursor.execute("UPDATE nagios_alerts SET status='SENT', date_sent = NOW() where id= {0}".format(alert_id))
-            self.logger.debug("Update alert id {0} in the db for alerts".format(alert_id))
-            # Send the message after we are sure the update occured
-            message_str += alert_text
-            # Add the alert to lists of alerts we can acknowledge along with a short string
-            if notification_type == 'PROBLEM':
-                self.key_to_alert_id_mapping.append((alert_id,"{0};{1}".format(host_name, service_name)))
-            if index >= 4:
-                # Send 5 messages together at a time and then wait till the next call
-                break
+
+        message_str = """"""
+        for one_alert in unsent_alerts: # No need to check count as server limits it
+            url = 'http://{0}/update_alert/{1}/SENT'.format(hostname, alert_id)
+            r = requests.get(url)
+            if r.status_code != 200:
+                bot.sendMessage(chat_id=sender_id, text='An exception occured while updating alert status', reply_keyboard=None)
+
+            host_name = one_alert['hostname']
+            service_name = one_alert['hostname']
+            message_str += one_alert['message_text']
+            if one_alert['acknowledgeable']:
+                acknowledgeable_alerts_cache[one_alert['alert_id']] = (host_name, service_name) # Add to dictionary to track
             message_str += "--------------------\n"
-            self.logger.info(alert_text)
 
         if message_str:
-            message_str += "{0}/{1} messages sent".format(index+1, total_count_of_alerts) # Since we start at 0
+            message_str += "{0} messages sent".format(len(unsent_alerts))
             bot.sendMessage(chat_id=admin_id, text=message_str)
-            # Send message with options to acknowledge alerts
-            if self.key_to_alert_id_mapping: # We have some alerts that can be acknowledged
-                acknowledge_string = """ALERTS THAT CAN BE ACKNOWLEDGED: \n"""
-                options = [] # Stores the keys for the keyboard reply
-                for index, one_alert in enumerate(self.key_to_alert_id_mapping):
-                    key_string = ' '.join(['/acknowledge',str(index)])
-                    options.append([ key_string ]) # Store the key for the keyboard
-                    acknowledge_string += "{0} : {1} \n".format(index, one_alert[1])
-
-                # Send the message with the keyboard
-                reply_keyboard = ReplyKeyboardMarkup(options, one_time_keyboard=True)
-                bot.sendMessage(chat_id=admin_id, text=acknowledge_string, reply_markup=reply_keyboard)
 
 
+        if acknowledgeable_alerts_cache: # We have some alerts that can be acknowledged
+            acknowledge_string = """ALERTS THAT CAN BE ACKNOWLEDGED: \n"""
+            options = []  # Stores the keys for the keyboard reply
+            for alert_id, (host, service) in acknowledgeable_alerts_cache.items():
+                key_string = "acknowledge {alert_id} | {host}  {service}".format(alert_id=alert_id, host=host, service=service)
+                options.append([ key_string ]) # Store the key for the keyboard
 
-            self.logger.info("Finished sending alerts")
+            # Send the message with the keyboard
+            reply_keyboard = ReplyKeyboardMarkup(options, one_time_keyboard=True)
+            bot.sendMessage(chat_id=admin_id, text=acknowledge_string, reply_markup=reply_keyboard)
+
+        self.logger.info("Finished sending alerts")
         # Commit changes and close db
-        conn.commit()
-        conn.close()
 
     def power_status(self, bot, update, args):
         arguments_to_use = ['status', 'timeleft']
@@ -158,53 +140,35 @@ class TelegramBot(object):
     
 
     def acknowledge_alert(self, bot, update, args):
-        """Takes the given alert and sends a request to acknowledge it.
-        For now we just schedule 1 day of downtime so that it is not forgotten
         """
-        self.logger.info("Got request to acknolwedge id {0}".format(args), sender_id=update.message.chat_id)
+        Given a string in the form of "acknowledge <ID> | <SOME DESC>" this sends the appropriate nagios command
+        :param bot:
+        :param update:
+        :param args:
+        :return:
+        """
+        self.logger.info("Got request to acknowledge id {0}".format(args), sender_id=update.message.chat_id)
         if not args:
             # did not pass us an alert id
             bot.sendMessage(chat_id=update.message.chat_id, text="No alert specified")
             return ConversationHandler.END
 
         try:
-            alert_id, _ = self.key_to_alert_id_mapping[int(args[0])]
+            acknowledge_string_and_id, _ = args[0].split('|')
+            _, alert_id = acknowledge_string_and_id.split(' ').strip()
         except IndexError:
             # Some how the key they sent does not exist
-             bot.sendMessage(chat_id=update.message.chat_id, text="Key does not exist")
+             bot.sendMessage(chat_id=update.message.chat_id, text="Invalid string passed to acknowledge")
              return ConversationHandler.END
-        # Find the alert id if it exists
-        # Open the database
-        db_host_name = self.config.get('DATABASE', 'host')
-        db_user_name = self.config.get('DATABASE', 'user')
-        db_password = self.config.get('DATABASE', 'password')
-        database_name = self.config.get('DATABASE', 'database')
 
-        conn =  mysql.connector.connect(user=db_user_name,password=db_password,host=db_host_name, database=database_name)
-        cursor = conn.cursor(buffered=True)
-        query = """SELECT hostname, service_name FROM nagios_alerts where id=%s"""
-        cursor.execute(query, (alert_id,))
+        hostname = self.config.get('ALERTS', 'hostname')
+        url = 'http://{0}/acknowledge/{1}'.format(hostname, alert_id)
+        r = requests.get(url)
+        if r.status_code != 200:
+            bot.sendMessage(chat_id=sender_id, text='An exception occured while acknowledging alert', reply_keyboard=None)
+            return ConversationHandler.END
 
-        # For now we use the downtime command to make sure the alert is not forgotten about
-        current_time = int(time.time()) # Time in linux seconds since it doesnt make much of a difference for 1 second
-        one_day = 60 * 60 * 24 # represents 24 hours
-        end_time = current_time + one_day # represents 24 hours
-
-        #TODO: This needs to be cleaned and commented more
-        result = cursor.fetchone()
-        command_string = """[{0}] """.format(current_time)
-        host_name = result[0]
-        service_name = result[1]
-        ACK_COMMAND = 'SCHEDULE_SVC_DOWNTIME'
-        COMMENT_COMMAND = 'ADD_SVC_COMMENT'
-        comment = 'DOWNTIME SCHEDULED VIA TELEGRAM'
-        host_and_service_name = ';'.join([host_name, service_name])
-        command_string += ';'.join([ACK_COMMAND, host_and_service_name, str(current_time), str(end_time), '1', '0', str(one_day), 'nagiosadmin', comment])
-        comment_string = """[{0}] {1};{2}""".format(current_time, COMMENT_COMMAND, ';'.join([host_and_service_name, '1', 'nagiosadmin', comment]))
-        command_to_run = """echo '{0}' > /var/lib/nagios3/rw/nagios.cmd"""
-        text_output = subprocess.check_output(command_to_run.format(command_string), shell=True)
-        text_output = subprocess.check_output(command_to_run.format(comment_string), shell=True)
-        bot.sendMessage(chat_id=update.message.chat_id, text="Marked {0} for 1 day of downtime".format(host_and_service_name))
+        bot.sendMessage(chat_id=sender_id, text='Successfully scheduled downtime for 1 day', reply_keyboard=None)
 
         return ConversationHandler.END
 
@@ -447,7 +411,7 @@ class TelegramBot(object):
         power_status_handler = CommandHandler('powerstatus', self.power_status, pass_args=True)
         self.dispatcher.add_handler(power_status_handler)
 
-        acknowledge_alert_handler = CommandHandler('acknowledge', self.acknowledge_alert, pass_args=True)
+        acknowledge_alert_handler = RegexHandler('acknowledge', self.acknowledge_alert, pass_args=True)
         self.dispatcher.add_handler(acknowledge_alert_handler)
 
         # Handler for opening the garage
