cc1690380bbd2e161a4a6cf9e8e85404ca74e12a
Avi Oza <avioza@gmail.com>
2018-12-29 22:29:55 -0500

    add parsing for the result returned back from the queue
    Signed-off-by: Avi Oza <avioza@gmail.com>

--- telegram_bot/garage_door.py
+++ telegram_bot/garage_door.py
@@ -2,6 +2,7 @@ import requests
 import logging
 import boto3
 import json
+import datetime
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG) #TODO: Remove this later on
@@ -18,28 +19,44 @@ class GarageDoorHandler:
         self.garage_door_user_pass = (user, password)
 
         # Queue where we will listen for responses on
-        self.sqs = boto3.resource('sqs')
+        self.sqs = boto3.client('sqs')
         self.sns = boto3.client('sns')
-        # self._garage_request_arn = '' #TODO: Fill this in via config
-        # self._garage_response_queue = sqs.get_queue_by_name(QueueName='garage-responses')
+        self._garage_request_arn = config.get('GARAGE', 'request_arn')
+        self._garage_response_queue_url = self.sqs.get_queue_url(QueueName='garage-responses')['QueueUrl']
 
 
-
-    def _get_garage_position(self, garage_name='all'):
+    def _get_garage_position(self, garage_name='ALL'):
         # Returns whether the garage is open or closed
-        # message = {'type': 'STATUS'}
-        # response = self.sns.publish_message(TargetArn=self._garage_request_arn,
-        #                          Message=json.dumps({'default': json.dumps(message)}),
-        #                          Subject='Garage Position Request',
-        #                          MessageStructure='json')
-        request_url = '{0}/garage/status/{1}'.format(self.garage_door_base_url, garage_name)
-        logger.info("Requesting garage status from url".format(request_url))
-        r = requests.get(request_url, auth=self.garage_door_user_pass)
-        if r.status_code == 200:
-            logger.info("Got response as {}".format(r.json()))
-            return r.json()
+        message = {'type': 'STATUS', 'garage_name': garage_name}
+        response = self.sns.publish(TargetArn=self._garage_request_arn,
+                                 Message=json.dumps({'default': json.dumps(message)}),
+                                 Subject='Garage Position Request',
+                                 MessageStructure='json')
+        logger.info("Response for request was {}".format(response))
+        # To make sure the response goes with the request
+        expected_return_id = response['MessageId'][:4]
 
-        logger.info("Got response as {}".format(r.json()))
+        logger.info("Sent request for garage status: {}. Waiting for response".format(response))
+        logger.info("Expecting return message_id: {}".format(expected_return_id))
+
+        responses = self.sqs.receive_message(QueueUrl=self._garage_response_queue_url, MaxNumberOfMessages=10, WaitTimeSeconds=10)
+
+        if 'Messages' not in responses:
+            logger.error("Did not receive a response about garage status")
+            return []
+
+        for response in responses['Messages']:
+            garage_statuses = json.loads(response['Body'])
+            # check if this was the request we sent
+            if garage_statuses['id'] != expected_return_id:
+                continue
+
+            logger.info("Received response for the garage as {}".format(garage_statuses))
+            # delete message from the queue
+            self.sqs.delete_message(QueueUrl=self._garage_response_queue_url, ReceiptHandle=response['ReceiptHandle'])
+            return garage_statuses['status']
+
+        logger.error("Did not get any expected responses")
         return []
 
     def _control_garage(self, garage_name, action):
@@ -56,11 +73,12 @@ class GarageDoorHandler:
         :return: str
         """
         return_message = ""
+        current_time = datetime.datetime.now().strftime('%Y-%m-%d %I:%M:%S %p')
 
         for one_garage_dict in garage_statuses:
             garage_name = one_garage_dict['garage_name']
             current_status = one_garage_dict['status']
-            return_message += '{:<5} : {}'.format(garage_name, ' '.join([current_status, one_garage_dict['status_time']])) + '\n'
+            return_message += '{:<5} : {}'.format(garage_name, ' '.join([current_status, current_time])) + '\n'
 
         return return_message
 
@@ -83,12 +101,3 @@ class GarageDoorHandler:
         options.append(["CANCEL GARAGE"])  # Store the key for the keyboard
 
         return options
-
-# if __name__ == '__main__':
-#     import configparser
-#     config = configparser.ConfigParser()
-#     config.read('/etc/telegram-bot/bot.config')
-#     g = GarageDoorHandler(config)
-#     print(g.status_to_string(status_str))
-#     # print(g.get_keyboard_format(status_str))
-#     pass
