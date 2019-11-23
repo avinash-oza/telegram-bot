038c86c1bf6344fc785273fa05bed33f9abd0de2
Avi Oza <avioza@gmail.com>
2019-11-23 10:38:22 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='telegram-bot',
-        version='0.2b9',
+        version='0.3.2',
         packages=['telegram_bot',],
         license='TBD',
         long_description='TBD'
