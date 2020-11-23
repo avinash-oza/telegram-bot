389051569b7927409943ad9502aa6314597cd71e
Avi <avioza@gmail.com>
2020-11-22 22:25:54 -0500

    add get method to expose config

--- telegram_bot/config_util.py
+++ telegram_bot/config_util.py
@@ -9,20 +9,26 @@ class ConfigHelper:
     def __init__(self, env_name):
         self._db = boto3.client('dynamodb')
         self._env_name = env_name.lower()
+        self.__config = None
 
-    def get_config(self):
-        results = self._db.query(TableName='configTable',
-                KeyConditionExpression="lookup_key = :k_name",
-                ExpressionAttributeValues={
-                    ':k_name': {'S': f'telegram-bot+{self._env_name}'}
-                    })
-        if len(results['Items']) != 1:
-            raise ValueError("Got multiple configs for key")
-        config_str = results['Items'][0]['value']['S']
-        config = json.loads(config_str)
+    def get(self, section, value):
+        return self.config[section][value]
 
-        return config
+    @property
+    def config(self):
+        if self.__config is None:
+            results = self._db.query(TableName='configTable',
+                    KeyConditionExpression="lookup_key = :k_name",
+                    ExpressionAttributeValues={
+                        ':k_name': {'S': f'telegram-bot+{self._env_name}'}
+                        })
+            if len(results['Items']) != 1:
+                raise ValueError("Got multiple configs for key")
+            config_str = results['Items'][0]['value']['S']
+            self.__config = json.loads(config_str)
+        return self.__config
+            
 
 if __name__== '__main__':
     c = ConfigHelper('prod')
-    print(c.get_config())
+    print(c.get('telegram','bot_admins'))
