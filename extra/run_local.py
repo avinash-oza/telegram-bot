d3bcc6a79562c926b294e606dc79173645855f10
Avi <avioza@gmail.com>
2020-12-20 17:55:14 -0500

    add script to run locally in non async mode

---
+++ extra/run_local.py
@@ -0,0 +1,13 @@
+from telegram.ext import Updater
+from telegram_bot.handlers import setup_handlers
+from telegram_bot.config_util import ConfigHelper
+
+if __name__ == '__main__':
+    c = ConfigHelper()
+    TELEGRAM_TOKEN = c.get('telegram', 'api_key')
+
+    updater = Updater(token=TELEGRAM_TOKEN)
+    dispatcher = updater.dispatcher
+    setup_handlers(dispatcher)
+
+    updater.start_polling()
