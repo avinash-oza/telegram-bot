021eb5eafea275d631d2b5f73f360e2b8fc59599
Avi <avioza@gmail.com>
2023-07-04 09:00:59 -0400

    formatting

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-        name='telegram-bot',
-        version='0.3.2',
-        packages=find_packages(),
-        license='TBD',
-        long_description='TBD'
-        )
+    name="telegram-bot",
+    version="0.3.2",
+    packages=find_packages(),
+    license="TBD",
+    long_description="TBD",
+)
