55ce9c186e589a33204d45c41ce701d4e7135d7f
Avi <avioza@gmail.com>
2026-02-15 21:59:40 -0500

    update code to always download/reupload new token on event creation

--- src/telegram_bot/handlers/google_calendar_handler.py
+++ src/telegram_bot/handlers/google_calendar_handler.py
@@ -1,7 +1,11 @@
 import datetime
 import logging
 import re
+import tempfile
 
+import boto3
+from gcsa.event import Event
+from gcsa.google_calendar import GoogleCalendar
 from telegram import ReplyKeyboardMarkup, Update
 from telegram.ext import ContextTypes, ConversationHandler, MessageHandler, filters
 
@@ -19,7 +23,7 @@ EVENT_DATE_INPUT_TEXT = (
 
 
 class GoogleCalendarHandler(HandlerBase):
-    def _add_new_event(self, event_date, event_text):
+    def _add_new_event(self, event_date: str, event_text: str):
         """
 
         :param message_text: Handles a message in the following format:
@@ -27,28 +31,40 @@ class GoogleCalendarHandler(HandlerBase):
         and adds it to the configured calendar
         """
         logger.info(f"Got event_date={event_date} and event_text={event_text}")
-        #
-        # event_date = arrow.get(event_date, SUPPORTED_DATE_FORMATS)
-        # logger.info(f"Parsed {event_title=} and {event_date=}")
-        #
-        # calendar_config = config.config["calendar"]
-        # token = Credentials(
-        #     token=calendar_config["token"],
-        #     refresh_token=calendar_config["refresh_token"],
-        #     client_id=calendar_config["client_id"],
-        #     client_secret=calendar_config["client_secret"],
-        #     scopes=["https://www.googleapis.com/auth/calendar"],
-        #     token_uri="https://oauth2.googleapis.com/token",
-        # )
-        # cal = GoogleCalendar(
-        #     calendar_config["calendar_id"], credentials=token, save_token=False
-        # )
-        #
-        # event = Event(
-        #     summary=event_title, start=event_date.date(), end=event_date.date()
-        # )
-        #
-        # cal.add_event(event=event)
+
+        event_date_obj = datetime.datetime.strptime(event_date, "%m/%d/%Y")
+
+        calendar_config = config.config["calendar"]
+        calendar_id = calendar_config["calendar_id"]
+        bucket_name = calendar_config["credentials_bucket_name"]
+        bucket_path_token = calendar_config["credentials_token_path"]
+        bucket_path_credentials = calendar_config["credentials_path"]
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            s3_client = boto3.client("s3")
+            # download to temp dir
+            token_path = f"{temp_dir}/token.pickle"
+            s3_client.download_file(bucket_name, bucket_path_token, token_path)
+
+            credentials_path = f"{temp_dir}/credentials.json"
+            s3_client.download_file(
+                bucket_name, bucket_path_credentials, credentials_path
+            )
+
+            cal = GoogleCalendar(
+                calendar_id,
+                token_path=token_path,
+                credentials_path=credentials_path,
+            )
+
+            logger.info("Uploading refreshed token to S3")
+            s3_client.upload_file(token_path, bucket_name, bucket_path_token)
+
+        event = Event(
+            summary=event_text, start=event_date_obj.date(), end=event_date_obj.date()
+        )
+
+        cal.add_event(event=event)
 
         logger.info(f"Completed adding event {event_text} on {event_date} to calendar")
 
@@ -87,13 +103,14 @@ class GoogleCalendarHandler(HandlerBase):
     async def _validate_all_details(
         self, update: Update, context: ContextTypes.DEFAULT_TYPE
     ):
-        event_name = update.message.text
+        event_name = update.message.text.upper()
         context.user_data["event_name"] = event_name
         event_date = context.user_data["formatted_date"]
 
         await update.message.reply_text(
-            f"Adding event {event_name} on {event_date} to calendar. Type Yes to confirm.",
-            reply_markup=ReplyKeyboardMarkup([["Yes"]], one_time_keyboard=True),
+            f"Adding event {event_name} on {event_date} to calendar. Select Yes to confirm, No to change the date"
+            f" or type to change event details.",
+            reply_markup=ReplyKeyboardMarkup([["Yes"], ["No"]], one_time_keyboard=True),
         )
         return STATE_CONFIRMING
 
@@ -123,6 +140,10 @@ class GoogleCalendarHandler(HandlerBase):
                                 self._add_event,
                             ),
                             MessageHandler(
+                                filters.Regex("^No"),
+                                self._validate_date,
+                            ),
+                            MessageHandler(
                                 filters.Regex("^.+$"),
                                 self._validate_all_details,
                             ),
@@ -132,3 +153,9 @@ class GoogleCalendarHandler(HandlerBase):
                 },
             )
         ]
+
+
+if __name__ == "__main__":
+    # for testing
+    handler = GoogleCalendarHandler(ConfigHelper())
+    handler._add_new_event("2024-06-30", "Test Event")
